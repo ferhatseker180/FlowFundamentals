f03bcc2feb262999ae137ad2d8099f0060f36fcb
ferhatseker180 <ferhatseker180@gmail.com>
2023-09-08 20:40:13 +0300

    Flow ile temel seviyede Count Down Timer Uygulaması gerçekleştirildi.Flow operatörleri kullanıldı.

--- app/src/main/java/com/ferhatt/flow/MyViewModel.kt
+++ app/src/main/java/com/ferhatt/flow/MyViewModel.kt
@@ -1,8 +1,14 @@
 package com.ferhatt.flow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
 
@@ -16,5 +22,35 @@ class MyViewModel : ViewModel() {
             counter--
             emit(counter)
         }
+    }
+
+    init {
+        collectInViewModel()
+    }
+
+    private fun collectInViewModel(){
+
+        viewModelScope.launch {
+            countDownTimerFlow
+                .filter {
+                    it %3 == 0
+                }
+                .map {
+                    it + it
+                }
+
+                .collect{
+                println("Counter is : ${it}")
+            }
+        }
+
+       /*
+       Diğer yol...
+        countDownTimerFlow.onEach {
+            println(it)
+        }.launchIn(viewModelScope)
+
+        */
+
     }
 }
