d3b9f4ad468ec86a8827ea101c4dcbfcb866ad2b
ferhatseker180 <ferhatseker180@gmail.com>
2023-09-09 00:58:22 +0300

    Flow ile temel seviyede Count Down Timer Uygulaması gerçekleştirildi.Farklı Flow türleri ve operatörleri kullanıldı.

--- app/src/main/java/com/ferhatt/flow/MyViewModel.kt
+++ app/src/main/java/com/ferhatt/flow/MyViewModel.kt
@@ -1,8 +1,14 @@
 package com.ferhatt.flow
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
@@ -53,4 +59,31 @@ class MyViewModel : ViewModel() {
         */
 
     }
+
+    // Livedata Comparisons
+
+    private val _liveData = MutableLiveData<String>("KotlinLiveData")
+    val livedata : LiveData<String> = _liveData
+
+    fun changeLiveData(){
+        _liveData.value = "Live Data"
+    }
+
+    private val _stateFlow = MutableStateFlow("KotlinStateFlow")
+    val stateFlow = _stateFlow.asStateFlow()
+
+    fun changeStateFlow(){
+        _stateFlow.value = "State Flow"
+    }
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    fun changeSharedFlow(){
+        viewModelScope.launch {
+            _sharedFlow.emit("Shared Flow")
+        }
+
+    }
+
 }
